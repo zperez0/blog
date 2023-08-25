c3e4a76c667981b9fa8f59036c0b1ebb084dc703
Yazmin Perez <zoeperez333@gmail.com>
2023-08-25 14:32:30 -0700

    update routes to include edit and delete - add edit and update actions in article controller - setup edit form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,4 @@
 class ArticlesController < ApplicationController
-
   def show
     @article = Article.find(params[:id])
   end
@@ -12,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -20,8 +23,17 @@ class ArticlesController < ApplicationController
       # or
       redirect_to @article
     else
-      render 'new'
+      render "new"
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render "edit"
+    end
+  end
 end
